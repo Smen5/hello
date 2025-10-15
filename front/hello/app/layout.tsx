7b323b9435b464710408541df43389a9a91ec40d
brutalKim <132333588+brutalKim@users.noreply.github.com>
2025-10-15 21:45:54 +0900

    create next js

---
+++ front/hello/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Smen5 TroubleShooting Archive",
+  description: "Smen5'TroubleShooting Archive",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="content">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
