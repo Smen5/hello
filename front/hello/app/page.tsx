686450e270261bc3620e4ddc24017647c52c030b
brutalKim <132333588+brutalKim@users.noreply.github.com>
2025-10-15 22:05:20 +0900

    add text-area

--- front/hello/app/page.tsx
+++ front/hello/app/page.tsx
@@ -1,22 +1,25 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
+import SubmitBtn from "@/components/ui/submitBtn/SubmitBtn";
 
 export default function Home() {
   return (
     <div className={styles.textSection}>
-    <TextareaAutosize
-          minRows={3}
-          placeholder="내용을 입력해 주세요.."
-          style={{
-            padding: "5px",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-            fontSize: "16px",
-            outline: "none",
-            resize: "none",
-          }}
-        />
+      <TextareaAutosize
+        minRows={3}
+        placeholder="내용을 입력해 주세요"
+        style={{
+          padding: "0.5rem",
+          border: "1px solid #ccc",
+          borderRadius: "4px",
+          fontSize: "1rem",
+          outline: "none",
+          resize: "none",
+        }}
+      />
+      <div id={styles.submitBtn}>
+        <SubmitBtn/>
+      </div>
     </div>
   );
 }
---
+++ front/hello/components/ui/submitBtn/SubmitBtn.tsx
@@ -0,0 +1,9 @@
+import "./SubmitBtn.css";
+const SubmitBtn = () => {
+    return(
+        <button type="submit">
+            <img src="/send.svg" alt="Send" width={24} height={24} />
+        </button>
+    )	
+};
+export default SubmitBtn;
